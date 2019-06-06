0f8b2098638da7904062e793fd5ef8b6971c3800
Tyelcie <tyelcie@gmail.com>
2019-06-06 17:50:02 +0800

    workflow

--- model.py
+++ model.py
@@ -23,11 +23,28 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        captions = captions[:,:-1]
+        embd = self.embed(captions)
+        inputs = torch.cat((features.unsqueeze(1), embd), 1)
+        hiddens, _ = self.lstm(inputs)
+        outputs = self.linear(hiddens)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        smpl = []
+        for i in range(max_len):
+            hiddens, states = self.lstm(inputs, states)
+            outputs = self.linear(hiddens.squeeze(1))
+            predicted = outputs.argmax(1)
+            smpl.append(predicted.item())
+            
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        return smpl
